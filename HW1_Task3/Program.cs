e429af69f946878c18684ab850adbb1d99b5f628
Morphine-medicine <53969738+Morphine-medicine@users.noreply.github.com>
2020-09-30 21:21:05 +0300

    Add files via upload

---
+++ HW1_Task3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HW1_Task3
+{
+    class Converter
+    {
+        public double USD;
+        public double EU;
+        public Converter( double usd, double euro)
+        {
+            USD = usd;
+            EU = euro;
+        }
+        public double UAH_To_USD (double amount)
+        {
+            return (amount * (1 / USD));
+        }
+        public double UAH_To_EU(double amount)
+        {
+            return (amount * (1 / EU));
+        }
+        public double USD_To_UAH(double amount)
+        {
+            return (amount * USD);
+        }
+        public double EU_To_UAH(double amount)
+        {
+            return (amount * EU);
+        }
+        public double EU_To_USD_with_UAH(double amount)
+        {
+            return (UAH_To_USD(EU_To_UAH(amount)));
+        }
+        public double USD_To_EU_with_UAH(double amount)
+        {
+            return (UAH_To_EU(USD_To_UAH(amount)));
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter c1 = new Converter(28.31, 33.18);
+            Console.WriteLine(c1.UAH_To_EU(1));
+            Console.WriteLine(c1.UAH_To_USD(1));
+            Console.WriteLine(c1.USD_To_UAH(1));
+            Console.WriteLine(c1.EU_To_UAH(1));
+            Console.WriteLine(c1.USD_To_EU_with_UAH(1));
+            Console.WriteLine(c1.EU_To_USD_with_UAH(1));
+        }
+    }
+}
---
+++ HomeWork/Task2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace HomeWork
+{
+    abstract class Student
+    {
+        public readonly string name;
+        public string state;
+        public Student(string Name)
+        {
+            name = Name;
+            state = "";
+        }
+        public void Relax()
+        {
+            state += "\n Relax";
+        }
+        public void Read()
+        {
+            state += "\n Read";
+        }
+        public void Write()
+        {
+            state += "\n Write";
+        }
+
+        public abstract void Study();
+
+    }
+    class GoodStudent : Student
+    {
+        public GoodStudent(string name)
+            : base(name)
+        {
+            state += "Good \n";
+        }
+        override public void Study()
+        {
+            Read(); Write(); Read(); Write(); Relax();
+        }
+    }
+    class BadStudent : Student
+    {
+        public BadStudent(string name)
+            : base(name)
+        {
+            state += "Bad \n";
+        }
+        override public void Study()
+        {
+            Relax(); Relax(); Relax(); Relax(); Read();
+        }
+    }
+    class Group
+    {
+        public string groupName;
+        private List<Student> students;
+        public Group(string Name)
+        {
+            groupName = Name;
+            students = new List<Student>();
+        }
+        public Group(Student st1, Student st2, Student st3)
+        {
+            groupName = "Unkown";
+            students = new List<Student>() {st1, st2, st3 };
+        }
+        public void addStudent(Student st)
+        {
+            students.Add(st);
+        }
+        public void changeName(string Name)
+        {
+            groupName = Name;
+        }
+        public string GetInfo()
+        {
+            string result = groupName + ": ";
+            students.ForEach(delegate (Student st)
+            {
+                result += st.name + " ";
+            });
+            return result;
+        }
+        public string GetFullInfo()
+        {
+            string result = groupName + ": ";
+            students.ForEach(delegate (Student st)
+            {
+                result += "\n" +st.name + " " + st.state;
+            });
+            return result;
+        }
+
+    }
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            GoodStudent Vlasov = new GoodStudent("Micle");
+            GoodStudent Ave = new GoodStudent("Maria");
+            BadStudent Pupkin = new BadStudent("Vasya");
+            BadStudent Ivanov = new BadStudent("Ivan");
+            Group K25 = new Group(Ivanov, Pupkin, Vlasov);
+            Console.WriteLine(K25.GetInfo());
+            K25.changeName("K25");
+            Console.WriteLine(K25.GetFullInfo());
+            K25.addStudent(Ave);
+            Vlasov.Study();
+            Ave.Read();
+            Pupkin.Study();
+            Ivanov.Write();
+            Console.WriteLine(K25.GetFullInfo());
+        }
+    }
+}
